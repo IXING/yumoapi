3180239532b2196f2806f57180225b94c6503f93
IXING <982592221@qq.com>
2017-05-19 09:54:47 +0800

    删除无用信息

--- htdocs/Application/Gapi/Controller/BizhiController.class.php
+++ htdocs/Application/Gapi/Controller/BizhiController.class.php
@@ -2,7 +2,6 @@
 namespace Gapi\Controller;
 use Think\Controller;
 class BizhiController extends Controller {  
-	//修改php权限 
     public function index(){
     	$Data = M('bizhi');// 实例化Data数据模型
     	$cid = I('post.id');
